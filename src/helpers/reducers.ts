062cb0c20ab8e2a01b808988b8de6d9154eefed6
Tomáš Kuruc <tomkokuruc@gmail.com>
2022-01-29 14:37:49 +0100

    .

---
+++ src/helpers/reducers.ts
@@ -0,0 +1,47 @@
+import {combineReducers} from "redux";
+import {TypedUseSelectorHook, useSelector} from "react-redux";
+import ModalAction from "stores/modal/ModalAction";
+
+function modal(state: null | any, action: any) {
+    if (typeof state === "undefined") {
+        return {
+            component: null,
+        };
+    }
+    if (action.type === ModalAction.SET_OPEN_MODAL) {
+        if (action.toggle) {
+            if (state && state.component && state.component === action.component) {
+                return {
+                    component: null,
+                    props: {
+                        ...state.props,
+                        ...(action.props || {}),
+                    },
+                    isOpen: !state.isOpen,
+                };
+            }
+        }
+        const newState = action.component
+            ? {
+                isOpen: true,
+                ...action,
+            }
+            : {
+                component: null,
+                props: {},
+            };
+
+        return newState;
+    } else {
+        return state;
+    }
+}
+
+const reducers = combineReducers({
+    modal,
+});
+
+export type RootState = ReturnType<typeof reducers>;
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default reducers;
